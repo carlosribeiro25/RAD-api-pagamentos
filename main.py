cc76a6b254801d9f754a763842ee11e72b76bb00
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-19 08:02:58 -0300

    feat: implement main API functionality with payment processing and database connectio

---
+++ main.py
@@ -0,0 +1,132 @@
+import random
+from typing import Dict, Any
+import pymysql 
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
+
+# Inicialização da instancia no FastAPI
+
+app = FastAPI(
+    title="Sabor Rápido - API de Pagamentos",
+    description="Serviço REST desenvolvido sob metologia RAD para processamento de transações.",
+    version="1.0.0"
+)
+
+@app.get("/", summary="Status da API")
+def root():
+    return {"status": "online", "docs": "/docs"}
+
+# Conecxao com o database
+
+DB_CONFIG = {
+    "host": "127.0.0.1",
+    "user": "wilson",
+    "password": "university",
+    "database": "sabor_rapido_db",
+    "port": 3306,
+}
+
+def get_conection():
+    try:
+        conection = pymysql.connect(**DB_CONFIG)
+        return conection
+    except pymysql.MySQLError as error:
+        print(f"Erro de conexao com o SGBD:{error}")
+        return None
+    
+class SolicitacaoPagamento(BaseModel):
+    id_pedido: int = Field(..., description="Identificador numérico do pedido", gt=0)
+    valor_total: float = Field(..., description="Valor total da tansação financeira", gt=0)
+    metodo_pagamento: str = Field(..., description="Forma de pagamento utilizada EX(: Cartao, Pix, Dinheiro)")
+
+@app.post("/pagamentos", status_code=status.HTTP_201_CREATED, summary="Registrar e processar um pagamento")
+def processar_pagamento(pagamento:SolicitacaoPagamento) -> Dict[str, Any]:
+    id_pedido = pagamento.id_pedido
+    valor_total = pagamento.valor_total
+    metodo_pagamento = pagamento.metodo_pagamento
+
+    status_simulado = (
+        "Aprovado"
+        if metodo_pagamento.lower() =="dinheiro"
+        else random.choices(["Aprovado", "Recusado"], weights=[90, 10])[0]
+    )
+
+    conection = get_conection()
+    if not conection:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Nao foi possivel estabelecer conexão com o banco de dados de pagamentos."
+        )
+    
+    try: 
+        cursor = conection.cursor()
+        sql_insert= """
+            INSERT INTO PAGAMENTO(id_pedido, valor_total, metodo_pagamento, status_pagamento)
+            VALUES (%s, %s, %s, %s);
+            """
+        cursor.execute(
+            sql_insert, (id_pedido, valor_total, metodo_pagamento, status_simulado)
+        )
+
+        conection.commit()
+
+        return {
+            "mensagem": "Pagamento processado com sucesso!",
+            "id_pedido": id_pedido,
+            "status": status_simulado,
+            "valor-total": valor_total,
+        }
+
+    except pymysql.IntegrityError: 
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Já existe um registro de pagamento para o pedido ID{id_pedido}."
+        )
+    except pymysql.MySQLError as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Erro interno de processamento de banco de dados: {err}"
+        )
+    finally:
+        cursor.close()
+        conection.close()
+    
+@app.get("/pagamentos/{id_pedido}", summary="Consultar dados de um pagamento existente")
+def consultar_pagamento(id_pedido:int) -> Dict[str, Any]:
+    conection = get_conection()
+    if not conection:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Nao foi possivel estabelecer conexao com o banco de dados."
+        )
+    
+    try: 
+        cursor = conection.cursor(pymysql.cursors.DictCursor)
+
+        sql_select="SELECT * FROM PAGAMENTO WHERE id_pedido = %s;"
+        cursor.execute(sql_select,(id_pedido,))
+        registro = cursor.fetchone()
+
+        cursor.close()
+        conection.close()
+
+        if not registro:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Pagamento associado ao pedido {id_pedido} nao encontrado."
+            )
+        
+        return registro
+    except pymysql.MySQLError as err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Erro de comunicação com o a base de dados: {err}"
+        )
+        
+    
+
+    
+
+
+
+
